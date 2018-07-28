065aab4b4f94c9fd0be12181168f3e9d7f7db91d
git config --global user.name <2684165263@qq.com>
2018-07-28 12:01:34 +0800

    ex1 to 15

---
+++ ex10.py
@@ -0,0 +1,2 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line"
---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here are some new strange stuff, remember type ie exactly.
+
+days = "Mon Tue Wed Thr Fri Sat Sun "
+months = "Jun/nFeb/nMar/nApr/nMay/nJun/nJUl/nAug/nSep/nOct/nNov/nDec"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There are someting going on here.
+With three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6
+""")
